3710a9273af121a5078a801d00a3acf4ca14e765
nanda-java <nandakumar.a@pec.edu>
2023-08-01 11:41:21 +0530

    starting from scratch
    redesigning the website

--- inc/db_connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-$mysql_hostname = "localhost";
-$mysql_user = "root";
-$mysql_password = "";
-$mysql_database = "sharp_db";
-
-$db_connect = mysqli_connect($mysql_hostname, $mysql_user,$mysql_password, $mysql_database) or ("Could not connect database");
-
-?>
